ca7fb2b0eebf973ff83cd2197d6ce92cc327fe65
liaohw <liaohw@asiainfo-linkage.com>
2016-06-28 23:49:26 +0800

    update property

--- 41_Property.h
+++ 41_Property.h
@@ -1,11 +1,23 @@
-#import <Foundation/Foundation.h>
+//
+//  41_Property.h
+//  41_Property
+//
+//  Created by liaohw on 16/6/27.
+//  Copyright © 2016年 Asiainfo.com. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
 
 @interface CUser : NSObject {
 }
 
 //readonly： 不生成set方法， 只有get方法
+
 @property (nonatomic,copy,readwrite)NSString *name;  //注意括号里面的参数
 
 - (void)showInfo;
 
 @end
+
+#endif /* main_h */
--- 41_Property.m
+++ 41_Property.m
@@ -1,14 +1,23 @@
-#import <Foundation/Foundation.h>
-#import "41_Property.h"
+//
+//  41_Property.m
+//  41_Property
+//
+//  Created by liaohw on 16/6/27.
+//  Copyright © 2016年 Asiainfo.com. All rights reserved.
+//
 
-// 
+#import <Foundation/Foundation.h>
+#import "main.h"
 
 @implementation CUser
+
 //生成的属性名叫$name 而不是_name
 @synthesize name = $name;
+
 - (void)showInfo {
-    NSLog(@"用户名：%@",$name);
+    NSLog(@"showInfo 用户名：%@",$name);
 }
+
 @end
 
 int main(int argc, const char* argv[])
@@ -18,5 +27,11 @@ int main(int argc, const char* argv[])
     [user setName:@"admin"];
     
     [user showInfo];
+    NSLog(@"user.name ：%@",[user name]);
+    
     return 0;
 }
+
+//----------------引用计数:
+//showInfo 用户名：admin
+//user.name ：admin
