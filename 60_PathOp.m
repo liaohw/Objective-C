7e3404fde4a4b30e0a691c47320ac01cd1058a16
liaohw <liaohw@asiainfo-linkage.com>
2016-06-29 20:13:47 +0800

    update

--- 60_PathOp.m
+++ 60_PathOp.m
@@ -5,47 +5,57 @@
 
 int main(int argc, const char* argv[])
 {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
-
-
     //Home目录:
-    //模拟器 /Users/liaohw/Library/Application Support/iPhone Simulator/5.1/Applications/xxx  
+    //模拟器 /Users/liaohw/Library/Application Support/iPhone Simulator/5.1/Applications/xxx
     //真机 /var/mobile/Applications/xxx
-	NSString *homePath = NSHomeDirectory();  
-	NSLog(@"Home目录：%@", homePath);  
-
+    NSString *homePath = NSHomeDirectory();
+    NSLog(@"1.homePath：%@", homePath);
+    
     //1.返回路径的组成部分
     NSArray *array = [homePath pathComponents];
-    NSLog(@"pathComponents:%@",array);
+    NSLog(@"2.homePath.pathComponents:%@",array);
     
     //2.路径的最后组成部分
     NSString *lastComponent = [homePath lastPathComponent];
-    NSLog(@"lastComponent:%@",lastComponent);
+    NSLog(@"3.homePath.lastComponent:%@",lastComponent);
     
     //3.追加子路径
     NSString *newPath1 = [homePath stringByAppendingString:@"/README.md"];
     // NSString *newPath2 = [homePath stringByAppendingPathComponent:@"README.md"];
-    NSLog(@"newPath1=%@",newPath1);
+    NSLog(@"4.newPath1:%@",newPath1);
     
     
     //4.删除最后的组成部分
     NSString *deleteLast = [homePath stringByDeletingLastPathComponent];
-    NSLog(@"deleteLast:%@",deleteLast);
+    NSLog(@"5.homePath.deleteLast:%@",deleteLast);
     
     //5.删除扩展名
-    NSString *deleteExtension = [homePath stringByDeletingPathExtension];
-    NSLog(@"deleteExtension:%@",deleteExtension);
+    NSString *deleteExtension = [newPath1 stringByDeletingPathExtension];
+    NSLog(@"6.newPath1.deleteExtension:%@",deleteExtension);
     
     //6.获取路径最后部分的扩展名
-    NSString *extension = [homePath pathExtension];
-    NSLog(@"extension:%@",extension);
+    NSString *extension = [newPath1 pathExtension];
+    NSLog(@"7.newPath1.extension:%@",extension);
     
     //7.追加扩展名
     NSString *appendExt = [homePath stringByAppendingPathExtension:@"jpg"];
-    NSLog(@"appendExt:%@",appendExt);
-        
+    NSLog(@"8.homePath.appendExt:%@",appendExt);
 
-	[pool release];
+    
     return 0;
 }
+
+
+// 2016-06-29 20:04:16.461 oc_code1[2657:261204] 1.homePath：/Users/liaohw
+// 2016-06-29 20:04:16.462 oc_code1[2657:261204] 2.homePath.pathComponents:(
+//     "/",
+//     Users,
+//     liaohw
+// )
+// 2016-06-29 20:04:16.462 oc_code1[2657:261204] 3.homePath.lastComponent:liaohw
+// 2016-06-29 20:04:16.462 oc_code1[2657:261204] 4.newPath1:/Users/liaohw/README.md
+// 2016-06-29 20:04:16.462 oc_code1[2657:261204] 5.homePath.deleteLast:/Users
+// 2016-06-29 20:04:16.462 oc_code1[2657:261204] 6.newPath1.deleteExtension:/Users/liaohw/README
+// 2016-06-29 20:04:16.462 oc_code1[2657:261204] 7.newPath1.extension:md
+// 2016-06-29 20:04:16.462 oc_code1[2657:261204] 8.homePath.appendExt:/Users/liaohw.jpg
+// Program ended with exit code: 0
--- 61_FileOp.m
+++ 61_FileOp.m
@@ -5,26 +5,24 @@
 
 int main(int argc, const char* argv[])
 {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
-
+    
     // NSDate 数据的抽象，可存储音频、视频、图片、文本等数据
     NSLog(@"----------------NSDate----------------------------");
     NSString *init_s = @"simbababa";
     NSData *data = [init_s dataUsingEncoding:NSUTF8StringEncoding];
     NSString *initstr = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     NSLog(@"initstr = %@",initstr);
-
-    NSString *homePath = NSHomeDirectory();  
-    NSLog(@"Home目录：%@", homePath);  
-
-
-
+    
+    NSString *homePath = NSHomeDirectory();
+    NSLog(@"Home目录：%@", homePath);
+    
+    
+    
     // NSFileManager操作文件
     NSLog(@"----------------NSFileManager----------------------------");
     //追加子路径
     NSString *filePath = [homePath stringByAppendingPathComponent:@"oc.txt"];
-
+    
     //NSFileManager类设计为单实例，只能通过类方法defaultManager 创建
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
@@ -52,31 +50,31 @@ int main(int argc, const char* argv[])
     }
     //创建文件夹
     success = [fileManager createDirectoryAtPath:filePath2
-                   withIntermediateDirectories:YES
-                                    attributes:nil
-                                         error:&error];
+                     withIntermediateDirectories:YES
+                                      attributes:nil
+                                           error:&error];
     if (success) {
         NSLog(@"创建目录: %@",filePath2);
     } else {
         NSLog(@"目录创建失败: %@",error);
     }
-
+    
     //根据路径读取文件中的数据
     NSData *readdata = [fileManager contentsAtPath:filePath];
     NSString *string = [[NSString alloc] initWithData:readdata encoding:NSUTF8StringEncoding];
     NSLog(@"读取文件中的数据: %@",string);
-
-
+    
+    
     // writeToFile写文件
     NSString *hz = @"**liao";
     [hz writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
     NSLog(@"writeToFile数据: %@",hz); //覆盖原来的数据
-
-
-
+    
+    
+    
     // NSFileHandle读写文件内容，writeData写入数据
     NSLog(@"----------------NSFileHandle----------------------------");
-
+    
     NSFileHandle *writeHandle = [NSFileHandle fileHandleForWritingAtPath:filePath];
     //将文件的偏移量设置到末尾，写入文件时则从末尾开始写入
     [writeHandle seekToEndOfFile];
@@ -86,7 +84,7 @@ int main(int argc, const char* argv[])
     NSLog(@"seekToEndOfFile,writeData数据: %@",appendString);
     //关闭文件
     [writeHandle closeFile];
-
+    
     // 读写偏移
     NSLog(@"----------------读写偏移----------------------------");
     NSFileHandle *readHandle = [NSFileHandle fileHandleForReadingAtPath:filePath];
@@ -94,16 +92,31 @@ int main(int argc, const char* argv[])
     NSData *data2 = [readHandle readDataToEndOfFile];
     NSString *string2 = [[NSString alloc] initWithData:data2 encoding:NSUTF8StringEncoding];
     NSLog(@"%@",string2);
-
-
-
-    NSLog(@"----------------writeToFile----------------------------");
+    
+    
+    
+    NSLog(@"----------------initWithContentsOfFile--------------");
     // initWithContentsOfFile 读文件
     NSData *dataRd = [[NSData alloc] initWithContentsOfFile:filePath];
     NSLog(@"initWithContentsOfFile: %@",[[NSString alloc] initWithData:dataRd encoding:NSUTF8StringEncoding]);
-
-    [string release];
-    [string2 release];
-    [pool release];
+    
     return 0;
 }
+
+// 2016-06-29 20:07:43.854 oc_code1[2685:262627] ----------------NSDate----------------------------
+// 2016-06-29 20:07:43.855 oc_code1[2685:262627] initstr = simbababa
+// 2016-06-29 20:07:43.856 oc_code1[2685:262627] Home目录：/Users/liaohw
+// 2016-06-29 20:07:43.856 oc_code1[2685:262627] ----------------NSFileManager----------------------------
+// 2016-06-29 20:07:43.857 oc_code1[2685:262627] 创建文件：/Users/liaohw/oc.txt
+// 2016-06-29 20:07:43.858 oc_code1[2685:262627] 目录已经存在!!
+// 2016-06-29 20:07:43.858 oc_code1[2685:262627] 删除目录成功!!
+// 2016-06-29 20:07:43.863 oc_code1[2685:262627] 创建目录: /Users/liaohw/demo
+// 2016-06-29 20:07:43.863 oc_code1[2685:262627] 读取文件中的数据: simbababa
+// 2016-06-29 20:07:43.864 oc_code1[2685:262627] writeToFile数据: **liao
+// 2016-06-29 20:07:43.864 oc_code1[2685:262627] ----------------NSFileHandle----------------------------
+// 2016-06-29 20:07:43.864 oc_code1[2685:262627] seekToEndOfFile,writeData数据: hongwei
+// 2016-06-29 20:07:43.864 oc_code1[2685:262627] ----------------读写偏移----------------------------
+// 2016-06-29 20:07:43.864 oc_code1[2685:262627] liaohongwei
+// 2016-06-29 20:07:43.864 oc_code1[2685:262627] ----------------initWithContentsOfFile--------------
+// 2016-06-29 20:07:43.865 oc_code1[2685:262627] initWithContentsOfFile: **liaohongwei
+// Program ended with exit code: 0
