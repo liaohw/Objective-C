32c7f9659c75f8bcf00ed53fcb2dd249ac2c4c35
liaohw <liaohw@foxmail.com>
2016-07-16 18:57:37 +0800

    添加Mac应用实例

---
+++ CaseTool/CaseTool/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  CaseTool
+//
+//  Created by liaohw on 16/7/12.
+//  Copyright © 2016年 Asiainfo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
